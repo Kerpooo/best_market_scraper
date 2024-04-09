97c6a6afa49e645cba1b35410c6ab803c86dd8e0
Kerpooo <kevinrincon8@gmail.com>
2024-04-09 15:55:35 -0500

    refactor: Division del codigo en modulos
    Se divide el codigo en modulos para un manejo y lectura mas sencilla

--- main.py
+++ main.py
@@ -1,59 +1,16 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-import traceback
-import time
+from scraper import scraper, get_categorias
+from threading_scraper import thread_links
 
 
-## Setup chrome options
-chrome_options = Options()
-chrome_options.add_argument("--headless")  # Ensure GUI is off
-chrome_options.add_argument("--no-sandbox")
-
-
-def get_categorias(driver):
-    # Esperar hasta obtener el elemento específico
-    WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located(
-            (By.XPATH, '//a[contains(@href, "/supermercado")]')
-        )
-    )
-    categorias = driver.find_elements(By.XPATH, '//a[contains(@href, "/supermercado")]')
-
-    links_categorias = []  # Lista para almacenar los enlaces de categorías
-
-    for categoria in categorias:
-        link_categoria = categoria.get_attribute(
-            "href"
-        )  # Obtener el atributo href del enlace
-        links_categorias.append(link_categoria)  # Agregar el enlace a la lista
-
-    return links_categorias
-
-
-def scraper():
-    driver = webdriver.Firefox()
-    driver.get("https://www.tiendasjumbo.co/supermercado")
-    try:
-        # Esperar hasta que la pagina cargue
-        WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.TAG_NAME, "body"))
-        )
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-
-        get_categorias(driver)
-
-    except Exception as e:
-        print(e)
+initial_url = "https://www.tiendasjumbo.co/supermercado"
 
 
 def main():
-    scraper()
+    # Scraper de la pagina inicial
+    page_categorias = scraper(initial_url)
+    links_categorias = get_categorias(page_categorias)
+    page_categorias.quit()
+    thread_links(links_categorias)
 
 
 if __name__ == "__main__":
